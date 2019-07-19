3f8555bf05aa686f466d917d7a7ed0d98c8978a6
Jamie Vullo <jvullo@protonmail.com>
2019-07-19 01:21:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Select name
+  "Select name, SUM(character_id.)
     FROM characters
+    JOIN character_books 
+    ON 
     "
 end
